f152184be7e4f29362f0bed09b10f33767bcd2e4
Arijit Bhattacharya <hoodwink73@live.com>
2017-05-24 16:32:39 +0530

    Add interface for user to define how to extract items

--- src/Feed/actions/index.js
+++ src/Feed/actions/index.js
@@ -1,146 +1,142 @@
-import 'whatwg-fetch';
-import { FEED_REDUCER_KEY } from '../reducers';
+import 'whatwg-fetch'
+import { FEED_REDUCER_KEY } from '../reducers'
 
-export const REQUEST_FEED = 'REQUEST_FEED';
-export const SUCCESS_RECEIVE_FEED = 'SUCCESS_RECEIVE_FEED';
-export const ERROR_RECEIVE_FEED = 'ERROR_RECEIVE_FEED';
-export const UPDATE_PAGINATION = 'UPDATE_PAGINATION';
+export const REQUEST_FEED = 'REQUEST_FEED'
+export const SUCCESS_RECEIVE_FEED = 'SUCCESS_RECEIVE_FEED'
+export const ERROR_RECEIVE_FEED = 'ERROR_RECEIVE_FEED'
+export const UPDATE_PAGINATION = 'UPDATE_PAGINATION'
 
 export function requestFeedActionCreator(feedName, direction, endpoint) {
-  return {
-    type: REQUEST_FEED,
-    payload: {
-      feedName
-    },
-    meta: {
-      direction,
-      endpoint
-    }
-  };
+	return {
+		type: REQUEST_FEED,
+		payload: {
+			feedName
+		},
+		meta: {
+			direction,
+			endpoint
+		}
+	}
 }
 
 export function receiveFeedActionCreator(feedName, direction, items) {
-  return {
-    type: SUCCESS_RECEIVE_FEED,
-    payload: {
-      feedName,
-      items
-    },
-    meta: {
-      direction
-    }
-  };
+	return {
+		type: SUCCESS_RECEIVE_FEED,
+		payload: {
+			feedName,
+			items
+		},
+		meta: {
+			direction
+		}
+	}
 }
 
 export function errorFeedActionCreator(feedName, direction, error) {
-  return {
-    type: ERROR_RECEIVE_FEED,
-    meta: {
-      error: true,
-      direction
-    },
-    payload: {
-      feedName,
-      error
-    }
-  };
+	return {
+		type: ERROR_RECEIVE_FEED,
+		meta: {
+			error: true,
+			direction
+		},
+		payload: {
+			feedName,
+			error
+		}
+	}
 }
 
 function updatePaginationDetails(feedName, direction, url, hasMoreItems) {
-  return {
-    type: UPDATE_PAGINATION,
-    payload: {
-      feedName,
-      url,
-      hasMoreItems
-    },
-    meta: {
-      direction
-    }
-  };
+	return {
+		type: UPDATE_PAGINATION,
+		payload: {
+			feedName,
+			url,
+			hasMoreItems
+		},
+		meta: {
+			direction
+		}
+	}
 }
 
 function getPaginationDetails(state, feedName) {
-  return state[FEED_REDUCER_KEY].paginations[feedName];
+	return state[FEED_REDUCER_KEY].paginations[feedName]
 }
 
 export var fetchFeedThunkCreator = function(...config) {
-  var [feedName, direction, getEndpoint, updateEndpoint, hasMoreItems] = config;
+	var defaultGetItems = items => items
+	var [
+		feedName,
+		direction,
+		getItems = defaultGetItems,
+		getEndpoint,
+		updateEndpoint,
+		hasMoreItems
+	] = config
 
-  return function fetchFeedThunk(dispatch, getState) {
-    var paginationDetails = getPaginationDetails(getState(), feedName);
-    var endpoint = getEndpoint(paginationDetails, direction);
+	return function fetchFeedThunk(dispatch, getState) {
+		var paginationDetails = getPaginationDetails(getState(), feedName)
+		var endpoint = getEndpoint(paginationDetails, direction)
 
-    if (
-      paginationDetails &&
-      paginationDetails[direction] &&
-      !paginationDetails[direction].hasMoreItems
-    ) {
-      return;
-    }
+		if (
+			paginationDetails &&
+			paginationDetails[direction] &&
+			!paginationDetails[direction].hasMoreItems
+		) {
+			return
+		}
 
-    dispatch(requestFeedActionCreator(feedName, direction, endpoint));
+		dispatch(requestFeedActionCreator(feedName, direction, endpoint))
 
-    // // memoize results
-    // if (localStorage.getItem(`react-feed-playgroubd-${feedName}`)) {
-    //   let cachedItems = JSON.parse(
-    //     localStorage.getItem(`react-feed-playgroubd-${feedName}`)
-    //   );
-    //   dispatch(receiveFeedActionCreator(feedName, direction, cachedItems));
-    //   return;
-    // }
+		fetch(endpoint)
+			.then(function(response) {
+				if (!response.ok) {
+					throw new Error({
+						url: response.url,
+						message: 'Not a successful request'
+					})
+				}
 
-    fetch(endpoint)
-      .then(function(response) {
-        if (!response.ok) {
-          throw new Error({
-            url: response.url,
-            message: 'Not a successful request'
-          });
-        }
+				try {
+					var nextPageUrl = updateEndpoint(response.clone())
+				} catch (e) {
+					throw new Error('Update endpoint has a problem', e)
+				}
 
-        try {
-          var nextPageUrl = updateEndpoint(response);
-        } catch (e) {
-          throw new Error('Update endpoint has a problem', e);
-        }
+				try {
+					var _hasMoreItems = hasMoreItems(response.clone(), direction)
+				} catch (e) {
+					throw new Error(
+						'Your method to determine the existence of further pages is having a problem',
+						e
+					)
+				}
 
-        try {
-          var _hasMoreItems = hasMoreItems(response, direction);
-        } catch (e) {
-          throw new Error('Your method to determine the existence of further pages is having a problem', e);
-        }
+				// `updateEndpoint` and `hasMoreItems` which are provided
+				// by a user can return a Promise instead of a string or boolean
+				Promise.all([
+					Promise.resolve(nextPageUrl),
+					Promise.resolve(_hasMoreItems)
+				]).then(function([nextPageUrl, _hasMoreItems]) {
+					dispatch(
+						updatePaginationDetails(
+							feedName,
+							direction,
+							nextPageUrl,
+							_hasMoreItems
+						)
+					)
+				})
 
-        // `updateEndpoint` and `hasMoreItems` which are provided
-        // by a user can return a Promise instead of a string or boolean
-        Promise.all([
-          Promise.resolve(nextPageUrl),
-          Promise.resolve(_hasMoreItems)
-        ])
-        .then(function ([nextPageUrl, _hasMoreItems]) {
-          dispatch(
-            updatePaginationDetails(
-              feedName,
-              direction,
-              nextPageUrl,
-              _hasMoreItems
-            )
-          );
-        })
-
-
-
-        return response.json();
-      })
-      .then(function dispatchSuccesfulFetchAction(items) {
-        dispatch(receiveFeedActionCreator(feedName, direction, items));
-        // localStorage.setItem(
-        //   `react-feed-playground-${feedName}`,
-        //   JSON.stringify(items)
-        // );
-      })
-      .catch(function dispatchFailureFetchAction(error) {
-        dispatch(errorFeedActionCreator(feedName, direction, error));
-      });
-  };
-};
+				return response.clone().json()
+			})
+			.then(function dispatchSuccesfulFetchAction(items) {
+				items = getItems(items)
+				dispatch(receiveFeedActionCreator(feedName, direction, items))
+			})
+			.catch(function dispatchFailureFetchAction(error) {
+				dispatch(errorFeedActionCreator(feedName, direction, error.message))
+			})
+	}
+}
--- src/Feed/containers/index.js
+++ src/Feed/containers/index.js
@@ -1,54 +1,55 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { fetchFeedThunkCreator } from '../actions';
-import { FEED_REDUCER_KEY } from '../reducers';
-import Feed from './Feed';
+import React from 'react'
+import { connect } from 'react-redux'
+import { fetchFeedThunkCreator } from '../actions'
+import { FEED_REDUCER_KEY } from '../reducers'
+import Feed from './Feed'
 
 var mapStateToProps = function(state, ownProps) {
-  var feedName = ownProps.name;
-  var isFetching = state.feeds.paginations[feedName]
-    ? state.feeds.paginations[feedName].below.isFetching
-    : false;
-  var moreItems = state.feeds.paginations[feedName]
-    ? state.feeds.paginations[feedName].below.hasMoreItems
-    : false;
+	var feedName = ownProps.name
+	var isFetching = state.feeds.paginations[feedName]
+		? state.feeds.paginations[feedName].below.isFetching
+		: false
+	var moreItems = state.feeds.paginations[feedName]
+		? state.feeds.paginations[feedName].below.hasMoreItems
+		: false
 
-  // if the feed has been already mounted, our
-  // redux store will have an corresponding state slice
-  // for the feed
-  if (state[FEED_REDUCER_KEY].entities[feedName]) {
-    return {
-      items: state.feeds.entities[feedName],
-      isFetching,
-      moreItems,
-      ...ownProps
-    };
-  } else {
-    return {
-      items: [],
-      isFetching,
-      moreItems,
-      ...ownProps
-    };
-  }
-};
+	// if the feed has been already mounted, our
+	// redux store will have an corresponding state slice
+	// for the feed
+	if (state[FEED_REDUCER_KEY].entities[feedName]) {
+		return {
+			items: state.feeds.entities[feedName],
+			isFetching,
+			moreItems,
+			...ownProps
+		}
+	} else {
+		return {
+			items: [],
+			isFetching,
+			moreItems,
+			...ownProps
+		}
+	}
+}
 
 var mapDispatchToProps = function(dispatch, ownProps) {
-  var feedName = ownProps.name;
-  var { getEndpoint, updateEndpoint, hasMoreItems } = ownProps;
-  return {
-    fetchFeed(direction = 'initial') {
-      dispatch(
-        fetchFeedThunkCreator(
-          feedName,
-          direction,
-          getEndpoint,
-          updateEndpoint,
-          hasMoreItems
-        )
-      );
-    }
-  };
-};
+	var feedName = ownProps.name
+	var { getItems, getEndpoint, updateEndpoint, hasMoreItems } = ownProps
+	return {
+		fetchFeed(direction = 'initial') {
+			dispatch(
+				fetchFeedThunkCreator(
+					feedName,
+					direction,
+					getItems,
+					getEndpoint,
+					updateEndpoint,
+					hasMoreItems
+				)
+			)
+		}
+	}
+}
 
-export default connect(mapStateToProps, mapDispatchToProps)(Feed);
+export default connect(mapStateToProps, mapDispatchToProps)(Feed)
